8419a81f406aab8c092dfe012fda2f986926853f
vijay dhyani <vijay.dhyani@ballisticlearning.com>
2022-11-01 15:26:34 +0530

    mobile fixes

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
-const bill = document.querySelector('.bill-input')
-const tip = document.querySelector('.tip-input')
-const people = document.querySelector('.people-input')
-const calbtn = document.querySelector('.calculate')
+const bill = document.querySelector(".bill-input");
+const tip = document.querySelector(".tip-input");
+const people = document.querySelector(".people-input");
+const calbtn = document.querySelector(".calculate");
+const result = document.querySelector(".result");
+const totalTip = document.querySelector(".totalTip");
+calbtn.onclick = function () {
+  billValue = parseInt(bill.value);
+  tipValue = parseInt(tip.value);
+  TotalBill = (billValue + (billValue * tipValue) / 100) / people.value;
+  //   console.log(TotalBill);
+  result.innerHTML = `₹ ${TotalBill.toFixed(2)}`;
+  tipAmount = tipValue / people.value;
+  //   console.log(tipAmount);
+  totalTip.innerHTML = `₹ ${tipAmount.toFixed(2)}`;
+};
 
-calbtn.onclick = function(){
-    billValue = parseInt(bill.value)
-    tipValue = parseInt(tip.value)
-    TotalBill =  (billValue+(billValue * tipValue/100))/people.value
-    console.log(TotalBill)
-}
+// bill.onkeyup = function () {
+//   console.log(bill.value);
+//   result.innerHTML = `${bill.value}`;
+// };
+
+document.querySelector(".reset").onclick = function () {
+  bill.value = "";
+  tip.value = "";
+  people.value = "";
+  result.innerHTML = "₹ 0.00";
+  totalTip.innerHTML = "₹ 0.00";
+};
