0b9564f384bf62390bcb3a8255a1f6ae628f30a4
vijay dhyani <vijay.dhyani@ballisticlearning.com>
2022-11-01 17:54:30 +0530

    required field condition added

--- script.js
+++ script.js
@@ -4,15 +4,21 @@ const people = document.querySelector(".people-input");
 const calbtn = document.querySelector(".calculate");
 const result = document.querySelector(".result");
 const totalTip = document.querySelector(".totalTip");
+
 calbtn.onclick = function () {
   billValue = parseInt(bill.value);
   tipValue = parseInt(tip.value);
   TotalBill = (billValue + (billValue * tipValue) / 100) / people.value;
   //   console.log(TotalBill);
-  result.innerHTML = `₹ ${TotalBill.toFixed(2)}`;
   tipAmount = tipValue / people.value;
   //   console.log(tipAmount);
-  totalTip.innerHTML = `₹ ${tipAmount.toFixed(2)}`;
+
+  if (bill.value == "" || tip.value == "" || people.value == "") {
+    return alert("Missing Value");
+  } else {
+    result.innerHTML = `₹ ${TotalBill.toFixed(2)}`;
+    totalTip.innerHTML = `₹ ${tipAmount.toFixed(2)}`;
+  }
 };
 
 // bill.onkeyup = function () {
